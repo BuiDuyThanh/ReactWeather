c42268e259126a8d6e565fed27e562d62d7c16a6
BuiDuyThanh <buiduythanh6291taikhoan@gmail.com>
2017-05-17 16:03:03 +0300

    Update middleware for localhost

--- server.js
+++ server.js
@@ -4,11 +4,22 @@ var express = require('express');
 var app = express();
 const PORT = process.env.PORT || 3000; //3000 is port number, process.env.PORT is environment variable's port
 
-app.use(function(req, res, next){						//express middleware
-	if (req.headers['x-forwarded-proto'] === 'http') {
+/*
+app.use(function(req, res, next){							// express middleware
+	if (req.headers['x-forwarded-proto'] === 'http') {		// req.headers['x-forwarded-proto'] doesn't exist locally
 		next();
 	} else {
+		res.redirect('http://' + req.hostname + req.url);	// so this line always run --> cannot access localhost
+	}
+});
+*/
+
+// update middleware
+app.use(function(req, res, next){
+	if (req.headers['x-forwarded-proto'] === 'https') {
 		res.redirect('http://' + req.hostname + req.url);
+	} else {
+		next();
 	}
 });
 
